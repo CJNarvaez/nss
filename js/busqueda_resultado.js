e894145b59dc91b6de1860a258b7907c7a915cd3
Cristhian Narvaez <cristhiannarvaez@gmail.com>
2015-03-20 12:23:15 -0600

    Revert "Inicio"
    This reverts commit d4005ff41525a8787bf7f9bbed8a011312f868c6.

--- js/busqueda_resultado.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-   //alert('funciona');
-   $("#btn_buscar").toggle();
-   $('#buscando').toggle();
-});
-function ocultar_mostrar()
-{
-    $('.folioFamVacio').parent().toggle();        
-};
