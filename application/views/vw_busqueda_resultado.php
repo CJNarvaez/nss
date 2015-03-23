65e485bbaba7005c8b0df8f870db7de3a9b7ebb3
Cristhian Narvaez <cristhiannarvaez@gmail.com>
2015-03-23 14:13:31 -0600

    agregar id
    Se agrego el id al boton VER/OCUL

--- application/views/vw_busqueda_resultado.php
+++ application/views/vw_busqueda_resultado.php
@@ -4,7 +4,7 @@
 <script type="text/javascript" src="../js/busqueda_resultado.js"></script>
 </head>
 <body>
-<div class="text-right"><input onclick="ocultar_mostrar()" class="btn btn-success btn-xs" type="button" value="VER / OCUL" /></div>
+<div class="text-right"><input id="btn_verOcul" onclick="ocultar_mostrar()" class="btn btn-success btn-xs" type="button" value="VER / OCUL" /></div>
 <div class="row"><?php echo $tabla; ?></div>
 </body>
 </html>
